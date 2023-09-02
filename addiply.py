0de8f3da0f94950a38e330f2169f97cdcd40e629
arn5891 <118304272+arn5891@users.noreply.github.com>
2023-09-02 16:40:39 -0400

    restore addiply.py

--- addiply.py
+++ addiply.py
@@ -1,1 +1,8 @@
-I LOST THE DANG CODE LIKE MAN WHO DOES THAT
+a =[]
+for i in range(int(input())):
+  line = input().split(" ")
+  b = int(line[0])+int(line[1])
+  c = int(line[0])*int(line[1])
+  a.append([b,c])
+for i in range(len(a)):
+  print(str(a[i][0])+" "+str(a[i][1]))
